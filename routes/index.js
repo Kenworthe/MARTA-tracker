9f78628a5f5897c09b3bff5023bc1069a22318bf
Kenneth Lin <kenworthe@gmail.com>
2017-01-04 16:36:02 -0500

    added signup form (incomplete)

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+/* POST SIGN UP */
+router.post('/', function(req, res, next) {
+  console.log('posting signup user:');
+  console.log(req.body);
+  res.redirect('/');
+});
+
 module.exports = router;
