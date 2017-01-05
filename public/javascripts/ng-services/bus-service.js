065d2ff45b15f096662e05f15ad624cf4c5d4819
Ramata Diallo <rama.diallo@gmail.com>
2017-01-05 17:11:04 -0500

    Successfully tested bus-service.js on browser

--- public/javascripts/ng-services/bus-service.js
+++ public/javascripts/ng-services/bus-service.js
@@ -1,1 +1,20 @@
-//hello ramata
+angular.module('martaApp').service('busService', function($http){
+  console.log('busService is alive!')
+  var busService;
+
+  $http({
+    method: 'GET',
+    url: 'http://developer.itsmarta.com/BRDRestService/RestBusRealTimeService/GetAllBus'
+  }).then(onBusShowSuccess, onError);
+})
+.controller('busController', function(busService){
+  console.log('busController is alive!')
+})
+var vm = this;
+function onBusShowSuccess(response){
+  console.log('here\'s the data for bus', response.data)
+  vm.bus = response.data;
+}
+function onError(error){
+  console.log('there was an error: ', error)
+}
