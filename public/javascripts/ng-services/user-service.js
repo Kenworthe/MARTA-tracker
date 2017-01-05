4c1e2f47620ec59c255dc5d800ed2957098e5a22
Van Moncrieff <vanmoncrieff@gmail.com>
2017-01-05 15:40:49 -0500

    first branch commit

--- public/javascripts/ng-services/user-service.js
+++ public/javascripts/ng-services/user-service.js
@@ -1,1 +1,14 @@
 //hello van
+// get user info ,
+// add favorites (post)
+// reorder favorites
+//delete favorites
+// UPDATE email address (with check for already taken)
+// UPDATE username (with check for already taken)
+// UPDATE password (with confirm password and current password)
+// DELETE user (close account, with client side validation)
+const app = angular.module('martaApp', []);
+
+app.service('userService', function($http) {
+  console.log('userService is working!');
+})
