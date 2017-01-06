5289d0accd702b0d67f3ea7a954044d3fc922ff4
Ramata Diallo <rama.diallo@gmail.com>
2017-01-06 12:08:40 -0500

    added ui-router(incomplete), removed pug(incomplete)

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,1 +1,49 @@
-angular.module('martaApp', []);
+angular.module('martaApp', ['ui.router'])
+.config(function($stateProvider, $urlRouterProvider, $locationProvider){
+  $stateProvider
+  .state('home', {
+    url: '/',
+    templateUrl: 'ng-templates/index.html',
+    controller: 'homeController',
+    controllerAs: '$ctrl'
+  })
+  .state('results', {
+    url: '/results',
+    templateUrl: '/ng-templates/results.html',
+    controller: 'resultsController',
+    controllerAs: '$ctrl'
+  })
+  .state('favorites', {
+    url: '/favorites',
+    templateUrl: '/ng-templates/favorites.html',
+    controller: 'favoritesController',
+    controllerAs: '$ctrl'
+  })
+  .state('profile', {
+    url: '/profile',
+    templateUrl: '/ng-templates/profile.html',
+    controller: 'profileController',
+    controllerAs: '$ctrl'
+  });
+  $urlRouterProvider
+  .otherwise('/');
+  $locationProvider
+  .html5Mode({ enabled: true, requireBase: false });
+
+})
+.controller('homeController', function() {
+  console.log('homeController is alive!');
+  this.title= 'Homepage';
+})
+.controller('resultsController', function() {
+  console.log('resultsController is alive!');
+  this.title= 'results page';
+})
+.controller('favoritesController', function() {
+  console.log('favoritesController is alive!');
+  this.title= 'favorites page';
+})
+.controller('profileController', function() {
+  console.log('profileController is alive!');
+  this.title= 'profile page';
+})
