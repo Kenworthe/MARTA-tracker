4d98298c579baa975d625404463b68b3d747dc2a
Kenneth Lin <kenworthe@gmail.com>
2017-01-09 14:43:28 -0500

    about to npm install request

--- public/javascripts/ng-services/rail-service.js
+++ public/javascripts/ng-services/rail-service.js
@@ -1,6 +1,16 @@
+// angular.module('martaApp')
+// 	.service('railService', function($http, $timeout){
+// 		this.getAllTrains = function(){
+// 			return $http.get('http://developer.itsmarta.com/RealtimeTrain/RestServiceNextTrain/GetRealtimeArrivals?apikey=e894d4a6-72ca-4268-94ec-af98560a3cc8');
+// 		}
+// 	})
+
 angular.module('martaApp')
 	.service('railService', function($http, $timeout){
 		this.getAllTrains = function(){
-			return $http.get('http://developer.itsmarta.com/RealtimeTrain/RestServiceNextTrain/GetRealtimeArrivals?apikey=e894d4a6-72ca-4268-94ec-af98560a3cc8');
+			return $http({
+				method: 'GET',
+				url: 'http://developer.itsmarta.com/RealtimeTrain/RestServiceNextTrain/GetRealtimeArrivals?apikey=e894d4a6-72ca-4268-94ec-af98560a3cc8'
+		});
 		}
 	})
