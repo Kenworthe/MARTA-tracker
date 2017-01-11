116d03c40cab56d5189639f04c0463abdece9583
Van Moncrieff <vanmoncrieff@gmail.com>
2017-01-11 10:03:39 -0500

    working on navbar.html

--- public/javascripts/ng-components/nav-bar.js
+++ public/javascripts/ng-components/nav-bar.js
@@ -1,6 +1,18 @@
 angular.module('martaApp')
 .component('navBar', {
-  templateUrl: '/javascripts/ng-templates/navbar.html',
-  controller: ,
+  templateUrl: '/javascripts/ng-templates/nav-bar.html',
+  controller: navBarController,
   controllerAs: '$ctrl'
 })
+function navBarController(userService){
+  var vm = this;
+  vm.user = {};
+  userService.getUser()
+  .then(function(response){
+    vm.user = response.data;
+    console.log(vm.user);
+  })
+  .catch(function(err){
+    console.log(err);
+  });
+}
