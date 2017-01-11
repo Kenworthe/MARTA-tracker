675563663621389947f139f323c181aea2d7965a
Kenneth Lin <kenworthe@gmail.com>
2017-01-11 18:00:22 -0500

    edited user-services.js

--- public/javascripts/ng-services/user-service.js
+++ public/javascripts/ng-services/user-service.js
@@ -9,22 +9,11 @@ angular.module('martaApp')
   }
 
   self.postFavorite = function(station){
-    // return $http.post('/users/:id', { favorites: station });
     return $http.post('/user', { favorites: station });
   }
 
   self.deleteFavorite = function(station){
-    // return $http.put('/users/:id', { favorites: station });
     return $http.put('/user', { favorites: station });
-  // self.getuserSelection = function(){
-  //   console.log('getting user selection...', self.userSelection);
-  //   return self.userSelection;
-  // }
-  // self.setUserSelection = function(newSelection){
-  //   console.log('current user selection is...', self.userSelection);
-  //   console.log('setting user stop to...', newSelection);
-  //   return self.userSelection = newSelection;
-  // }
   }
 })
 
@@ -41,9 +30,9 @@ angular.module('martaApp')
     .then(function(response){
       if (response.data.id){
         vm.user = response.data;
-        vm.favorites = response.data.favorites;
+        userService.userFavorites = response.data.favorites;
         console.log(vm.user);
-        console.log(vm.favorites);
+        console.log(userService.userFavorites);
       }
       else {
         console.log('response.data did not include user.id.\nPlease log in to continue.');
@@ -60,7 +49,6 @@ angular.module('martaApp')
     userService.postFavorite(station)
     .then(function(){
       vm.getUserInfo();
-      console.log('post successfull! here is the new user: ', vm.user.favorites)
     })
     .catch(function(err){
       console.log(err);
@@ -71,7 +59,6 @@ angular.module('martaApp')
     userService.deleteFavorite(station)
     .then(function(){
       vm.getUserInfo();
-      console.log("Here is the new", vm.user.favorites);
   })
     .catch(function(err){
       console.log(err);
