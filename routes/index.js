41c8b94562ad2812447f18dc721fa47ba232bdb4
Kenneth Lin <kenworthe@gmail.com>
2017-02-12 23:00:56 -0500

    removed require secret.js from app.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var request = require('request');
-var secret = require('../secret.js');
+// var secret = require('../secret.js');
 var User = require('../models/user');
 
 var martaRail = 'http://developer.itsmarta.com/RealtimeTrain/RestServiceNextTrain/GetRealtimeArrivals?apikey=';
